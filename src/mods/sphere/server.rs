11210e68aa515292f2e662ad672ca82c2d466e21
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-06-15 08:51:42 +0200

    Compact further

--- src/mods/sphere/server.rs
+++ src/mods/sphere/server.rs
@@ -13,10 +13,7 @@ pub fn main() {
     spawn_query(cube()).bind(|entities| {
         for (entity, _) in entities {
             remove_components(entity, &[&cube(), &cube_collider()]);
-            add_components(
-                entity,
-                make_sphere().with(sphere_collider(), 0.5), // .with_default(visualize_collider()),
-            );
+            add_components(entity, make_sphere().with(sphere_collider(), 0.5));
         }
     });
 }
--- src/server.rs
+++ src/server.rs
@@ -3,18 +3,16 @@ use ambient_api::{
     entity::add_component,
     prelude::*,
 };
+use itertools::Itertools;
 
 #[main]
 pub fn main() {
-    for y in 0..10 {
-        for x in 0..10 {
-            Entity::new()
-                .with_default(cube())
-                .with(cube_collider(), Vec3::ONE)
-                .with(translation(), vec3(x as f32 * 1.1, y as f32 * 1.1, 0.))
-                .with(color(), vec4(0.1, 0.1, 0.1, 1.))
-                .spawn();
-        }
+    for (x, y) in (0..10).cartesian_product(0..10) {
+        Entity::new()
+            .with_default(cube())
+            .with(cube_collider(), Vec3::ONE)
+            .with(translation(), uvec3(x, y, 0).as_vec3() * 1.1)
+            .spawn();
     }
     messages::Click::subscribe(|_, ev| {
         if let Some(hit) = physics::raycast_first(ev.origin, ev.dir) {
